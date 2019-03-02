d3ead9c9904996810159ba9d60f2ac998701cff3
Simon Walker <s.r.walker101@googlemail.com>
2019-03-02 23:21:22 +0000

    Correct tests

--- src/grid.rs
+++ src/grid.rs
@@ -21,7 +21,7 @@
 //! # let l1 = plotlib::line::Line::new(&[(0., 1.), (2., 1.5), (3., 1.2), (4., 1.1)])
 //! #    .style(plotlib::line::Style::new().colour("burlywood"));
 //! // let l1 = Line::new() ...
-//! let mut v = ContinuousView::new().add(&l1);
+//! let mut v = ContinuousView::new().add(l1);
 //!
 //! // 3 vertical lines and 8 horizontal lines
 //! v.add_grid(Grid::new(3, 8));
